295943c5750dce8db00c0d4e1fb2b22fd0fb2ba6
vicvqz <victoriavqz.2025@gmail.com>
2026-06-10 16:19:15 -0300

    código pruebas automatizadas

--- tests/books.test.js
+++ tests/books.test.js
@@ -1,3 +1,18 @@
-test("suma simple", () => {
-  expect(1 + 1).toBe(2);
+const request = require("supertest");
+
+const app = require("../server");
+
+describe("Book Catalog API", () => {
+
+    test("GET /books debe devolver la lista de libros", async () => {
+
+        const response = await request(app)
+            .get("/books");
+
+        expect(response.statusCode).toBe(200);
+
+        expect(Array.isArray(response.body)).toBe(true);
+
+    });
+
 });
