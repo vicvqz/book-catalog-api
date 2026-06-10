067fcffcf8065f3ea16ae000c67668d11dc690e5
vicvqz <victoriavqz.2025@gmail.com>
2026-06-10 15:19:08 -0300

    agregué nueva ruta POST

--- api/books.js
+++ api/books.js
@@ -8,4 +8,26 @@ router.get("/", (req, res) => {
     res.json(books);
 });
 
+router.post("/", (req, res) => {
+
+    const { title, author } = req.body;
+
+    if (!title || !author) {
+        return res.status(400).json({
+            error: "title y author son obligatorios"
+        });
+    }
+
+    const newBook = {
+        id: books.length + 1,
+        title,
+        author
+    };
+
+    books.push(newBook);
+
+    res.status(201).json(newBook);
+
+});
+
 module.exports = router;
